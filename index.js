119560173ed0f8c2929e8ae234a345241e6645e5
MikeYoulend <mikeyoulend@gmail.com>
2023-05-26 20:12:33 +0200

    Esercizio D2

---
+++ index.js
@@ -0,0 +1,34 @@
+
+/*Numeri = è un numero e si scrivono senza apici senò diventano stringhe
+  String = è una sequenza di caratteri, delimitati da degli apici
+  Boolean = può essere solo vera o falsa. ed è un'entità logica
+  Undefined = Rappresenta un valore che non esiste ancora, una sorta di gatto di schrödinger
+  Null = Rappresenta una valore nullo, la variabile non contiene nulla
+ */
+
+
+
+
+let x = 12
+let y = 20
+
+let name = "Michele"
+
+let somma = 12 + 20
+
+let sottrazione = x - 4
+
+console.log(sottrazione);
+
+let Studente = {
+    name: "Michele",
+    surname: "Lo Presti",
+    Age: "25",
+}
+
+console.log(Studente);
+
+let name1 = "john"
+let name2 = "John"
+
+console.log(name1 === name2.toLowerCase()); /* toLowerCase fa diventare i caratteri minuscoli */ 
